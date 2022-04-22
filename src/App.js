ea56c59dfc70c0b24ce9f86cb8f42eedd30f4cab
puleri <puleri.mp4@gmail.com>
2022-04-22 09:53:43 -0700

    v 0.4.3.B

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function App(props) {
   return (
     // <AuthProvider>
     <div className="App-wrapper">
-    <p style={{ zIndex: 99999, fontSize:'8px', position: 'absolute', top: '0', left: '4px', color: 'grey'}}> v 0.3.8.B</p>
+    <p style={{ zIndex: 99999, fontSize:'8px', position: 'absolute', top: '0', left: '4px', color: 'grey'}}> v 0.4.3.B</p>
     <AuthContextProvider>
 
         <Switch>
