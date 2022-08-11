b8860e1b379b3446e67ac675f678626a0f92e347
Matthew Puleri <mattpuleri@Matthews-MacBook-Pro.local>
2022-08-11 16:45:53 -0700

    local state updates and database update with waste factor form

--- src/components/Markets/CinciShingles.js
+++ src/components/Markets/CinciShingles.js
@@ -10,6 +10,10 @@ import 'firebase/firestore';
 export default function CinciShingles(props) {
     const [shingles, setShingles] = useState([]);
     const [edit, setEdit] = useState(null);
+    const [newMargin, setNewMargin] = useState({
+        waste: '',
+        mult: ''
+    });
 
     const handleAvail = (name, index) => {
         const db = firebase.firestore();
@@ -55,6 +59,72 @@ export default function CinciShingles(props) {
         })
         // setIsLoading(false)
     }
+    const handleSelect = (i) => {
+        setEdit(i)
+        setNewMargin({
+            waste: '',
+            mult: ''
+        })
+    }
+    const handleSubmit = () => {
+        const db = firebase.firestore();
+        const name = shingles[edit].name;
+
+        console.log(!newMargin.mult + " " + !newMargin.waste)
+
+        var docRef = db.collection("cin-shingles").doc(name);
+        let wastePercent;
+        let newState = [...shingles];
+
+        if (!newMargin.mult && !newMargin.waste) {
+            // end of function will reset form
+            // no need to here
+        } 
+        else if (!newMargin.waste) {
+            docRef.set({
+                multiplier: Number(newMargin.mult)
+            }, { merge: true }); 
+            newState[edit].multiplier = newMargin.mult
+        } 
+        else if (!newMargin.mult) {
+            wastePercent = (newMargin.waste * .01) + 1
+            docRef.set({
+                waste: wastePercent,
+            }, { merge: true }); 
+            newState[edit].waste = wastePercent
+        } 
+        else {
+            wastePercent = (newMargin.waste * .01) + 1
+            docRef.set({
+                waste: wastePercent,
+                multiplier: Number(newMargin.mult)
+            }, { merge: true }); 
+            newState[edit].multiplier = newMargin.mult
+            newState[edit].waste = wastePercent
+        }
+        
+        
+        // set local state to match DB
+        setShingles(newState)
+
+        // finally reset state and close inputs
+        setEdit(null)
+        setNewMargin({
+            waste: '',
+            mult: ''
+        })
+    }
+
+    const handleCancel = () => {
+        setEdit(null)
+        setNewMargin({
+            waste: '',
+            mult: ''
+        })
+    }
+    //edit holds index of edit
+    //submit changes in newMargin on update
+
     const shinglesIndex =
     shingles.map((shingle, i) =>
         <tr id={css.row} key={i}>
@@ -63,20 +133,18 @@ export default function CinciShingles(props) {
             <th className={css.name}>{shingle.name}</th>
             <th></th>
             <th></th>
-            <th></th>
-            <th></th>
             { (edit == i)
-            ? <th className={css.waste}><input id={css.multiInp} type='text' />&ensp;<i id={css.cash} class="fa-solid fa-calculator"></i></th>
-            : <th className={css.waste}>{shingle.multiplier}&ensp;<i id={css.cash} class="fa-solid fa-calculator"></i></th>
+            ? <th className={css.waste}><input id={css.multiInp} value={newMargin.mult} onChange={(e) => setNewMargin({ ...newMargin, mult: e.target.value })} type='text' />&ensp;<i id={css.cash} class="fa-solid fa-calculator"></i></th>
+            : <th className={css.waste}>{shingle.multiplier}<span id={css.x}>x</span><i id={css.cash} class="fa-solid fa-calculator"></i></th>
             }
 
             { (edit == i)
-            ? <th className={css.waste}> <input id={css.wasteInp} type='text' /> %<span className={css.wasteLabel}>WF</span></th>
-            : <th className={css.waste}> {(shingle.waste * 10 * 10 - 100)} %<span className={css.wasteLabel}>WF</span></th>
+            ? <th className={css.waste}> <input id={css.wasteInp} value={newMargin.waste} onChange={(e) => setNewMargin({ ...newMargin, waste: e.target.value })} type='text' /> %<span className={css.wasteLabel}>WF</span></th>
+            : <th className={css.waste}> {Math.round(shingle.waste * 10 * 10 - 100)} %<span className={css.wasteLabel}>WF</span></th>
             }
             { (edit == i) 
-            ? <th className={css.edit} onClick={() => setEdit(null)}><i class="fa-solid fa-circle-check"></i></th>
-            : <th className={css.edit} onClick={() => setEdit(i)}><i class="fa-solid fa-pen"></i></th>
+            ? <><th className={css.cancel} onClick={() => handleCancel()}><i class="fa-solid fa-rectangle-xmark"></i></th><th className={css.edit} onClick={() => handleSubmit()}><i class="fa-solid fa-circle-check"></i></th></>
+            : <th className={css.edit} onClick={() => handleSelect(i)}><i class="fa-solid fa-pen"></i></th>
             } 
        </tr>
     )
