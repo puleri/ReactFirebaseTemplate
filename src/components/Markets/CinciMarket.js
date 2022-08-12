d2671f74236c6fa0dbf3efea15cb0fe6201e4078
Matthew Puleri <mattpuleri@Matthews-MacBook-Pro.local>
2022-08-12 10:54:07 -0700

    create new shingle in cinn functional

--- src/components/Markets/CinciMarket.js
+++ src/components/Markets/CinciMarket.js
@@ -8,19 +8,26 @@ import NavMarket from './NavMarket.js';
 
 export default function CinciMarket(props) {
     const [category, setCategory] = useState('Shingles')
+    const [formOpen, setFormOpen] = useState(false)
     return (
         <>
             <Navbar/>
             <div className={css.container}>
-                <h2 className={css.header}>Cincinnati <span className='total-users'>{category}</span></h2>
+                <h2 className={css.header}>Cincinnati 
+                    <span className='total-users'>{category}</span>
+                </h2>
+                <button className={css.addNew} onClick={() => setFormOpen(true)}>+ Add New Shingle</button>
+
                 <div className={css.content}>
-                    <CinciShingles />
+                    <CinciShingles setFormOpen={setFormOpen} formOpen={formOpen}/>
+                    <div className={css.footer}>
+                    <Footer/>
+                    </div>
                 </div>
+                
             </div>
             <NavMarket setCategory={setCategory}/>
-            <div className={css.footer}>
-            <Footer/>
-            </div>
+            
         </>
     )
 }
--- src/components/Markets/CinciShingles.js
+++ src/components/Markets/CinciShingles.js
@@ -15,6 +15,19 @@ export default function CinciShingles(props) {
         mult: ''
     });
 
+    const [newData, setNewData] = useState({
+        available: false,
+        name: '',
+        multiplier: '',
+        waste: ''
+    })
+
+    const [notification, setNotification] = useState({
+        active: false,
+        error: false,
+        message: ''
+    })
+
     const handleAvail = (name, index) => {
         const db = firebase.firestore();
         let status;
@@ -114,7 +127,6 @@ export default function CinciShingles(props) {
             mult: ''
         })
     }
-
     const handleCancel = () => {
         setEdit(null)
         setNewMargin({
@@ -122,8 +134,84 @@ export default function CinciShingles(props) {
             mult: ''
         })
     }
-    //edit holds index of edit
-    //submit changes in newMargin on update
+
+    const handleCreateNew = () => {
+        let newWastePercent = (newData.waste* .01) + 1;
+        // on error
+        if (!newData.waste || !newData.name || !newData.multiplier) {
+
+            setNotification({
+                active: true,
+                error: true,
+                message: 'Fill out all fields before submitting'
+            })
+
+            setTimeout(() => {
+                setNotification({
+                    active: false,
+                    error: false,
+                    message: ''
+                })
+              }, "4000")
+              return
+        }
+        const db = firebase.firestore();
+        // Add a new document in collection "cities"
+        db.collection("cin-shingles").doc(newData.name).set({
+            name: newData.name,
+            available: newData.available,
+            waste: newWastePercent,
+            multiplier: Number(newData.multiplier)
+        })
+        .then(() => {
+            props.setFormOpen(false)
+            setNotification({
+                active: true,
+                error: false,
+                message: 'Material successfully added!'
+            })
+            setNewData({
+                available: false,
+                name: '',
+                multiplier: '',
+                waste: ''
+            })
+            setTimeout(() => {
+                setNotification({
+                    active: false,
+                    message: ''
+                })
+              }, "4000")
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+    }
+    const handleCancelForm = () => {
+        setNewData({
+            available: false,
+            name: '',
+            multiplier: '',
+            waste: ''
+        })
+        props.setFormOpen(false)
+    }
+
+    const newShingleForm = () => {
+        return (
+            <tr id={css.row}>
+                <th><label onClick={()=>setNewData({...newData, available: !newData.available})}className={css.toggle} for="myToggle"> <input className={css.toggle__input} name="" type="checkbox" id={css.myToggle} checked={newData.available}/><div className={css.toggle__fill}></div></label>
+                </th>
+                <th className={css.name}><input onChange={(e)=>setNewData({...newData, name: e.target.value})} value={newData.name} id={css.nameInp} type='text' /></th>
+                <th></th>
+                <th></th>
+                <th className={css.waste}><input onChange={(e)=>setNewData({...newData, multiplier: e.target.value})} value={newData.multiplier} id={css.multiInp} type='text' />&ensp;<i id={css.cash} className="fa-solid fa-calculator"></i></th>
+                <th className={css.waste}> <input onChange={(e)=>setNewData({...newData, waste: e.target.value})} value={newData.waste} id={css.wasteInp} type='text' /> %<span className={css.wasteLabel}>WF</span></th>
+                <th className={css.cancel}><i onClick={()=>handleCancelForm()} className="fa-solid fa-rectangle-xmark"></i></th><th className={css.edit}><i onClick={()=>handleCreateNew()} className="fa-solid fa-circle-check"></i></th>  
+            </tr>
+        )  
+    }
 
     const shinglesIndex =
     shingles.map((shingle, i) =>
@@ -134,8 +222,8 @@ export default function CinciShingles(props) {
             <th></th>
             <th></th>
             { (edit == i)
-            ? <th className={css.waste}><input id={css.multiInp} value={newMargin.mult} onChange={(e) => setNewMargin({ ...newMargin, mult: e.target.value })} type='text' />&ensp;<i id={css.cash} class="fa-solid fa-calculator"></i></th>
-            : <th className={css.waste}>{shingle.multiplier}<span id={css.x}>x</span><i id={css.cash} class="fa-solid fa-calculator"></i></th>
+            ? <th className={css.waste}><input id={css.multiInp} value={newMargin.mult} onChange={(e) => setNewMargin({ ...newMargin, mult: e.target.value })} type='text' />&ensp;<i id={css.cash} className="fa-solid fa-calculator"></i></th>
+            : <th className={css.waste}>{shingle.multiplier}<span id={css.x}>x</span><i id={css.cash} className="fa-solid fa-calculator"></i></th>
             }
 
             { (edit == i)
@@ -143,17 +231,22 @@ export default function CinciShingles(props) {
             : <th className={css.waste}> {Math.round(shingle.waste * 10 * 10 - 100)} %<span className={css.wasteLabel}>WF</span></th>
             }
             { (edit == i) 
-            ? <><th className={css.cancel} onClick={() => handleCancel()}><i class="fa-solid fa-rectangle-xmark"></i></th><th className={css.edit} onClick={() => handleSubmit()}><i class="fa-solid fa-circle-check"></i></th></>
-            : <th className={css.edit} onClick={() => handleSelect(i)}><i class="fa-solid fa-pen"></i></th>
-            } 
+            ? <><th className={css.cancel} onClick={() => handleCancel()}><i className="fa-solid fa-rectangle-xmark"></i></th><th className={css.edit} onClick={() => handleSubmit()}><i className="fa-solid fa-circle-check"></i></th></>
+            : <th className={css.edit} onClick={() => handleSelect(i)}><i className="fa-solid fa-pen"></i></th>
+            }
        </tr>
     )
     return (
         <>
-            <div className={css.key}><i id={css.cash} class="fa-solid fa-calculator"></i>&ensp; Cash multiplier &ensp; &ensp; | &ensp; &ensp; <span className={css.wasteKey}>WF &ensp;</span> Waste factor</div>
-           <div className={css.tableWrapper}>
-                {shinglesIndex}
-           </div>
+            { notification.active 
+            ? <div className={notification.error ? css.error : css.success}>{notification.message}</div>
+            : <></>
+            }
+            <div className={css.key}><i id={css.cash} className="fa-solid fa-calculator"></i>&ensp; Cash multiplier &ensp; &ensp; | &ensp; &ensp; <span className={css.wasteKey}>WF &ensp;</span> Waste factor</div>
+            <div className={css.tableWrapper}>
+                    {shinglesIndex}
+                    {props.formOpen ? newShingleForm() : <></>}
+            </div>
         </>
     )
 }
